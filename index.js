97bc0739f05d847f1ac038b11c21322e80368e04
intern <intern@admins-Mac-mini-2.local>
2019-02-11 18:26:52 +0530

    First

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-
-// io.on('connection',()=>{
-//   console.log('You are connected!');
-// }) 
-
-app.get('/',(request,response)=>{
-  response.sendFile(__dirname+"/page.html")
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + "/page.html")
 })
 
-io.on('connection',(socket)=>{
+// app.post('/',(req,res)=>{
+io.on('connection', (socket) => {
   console.log("User Connected!");
-  
-  socket.on('chat_message',(msg)=>{
-    console.log(`message: ${msg}` );
-    io.emit('chat',msg)
+  io.emit('chat',socket.handshake.address)
+  socket.on('chat_message', (msg) => {
+    console.log()
+    console.log(`message: ${msg}`);
+    io.emit('chat', msg)
   })
-  
-  socket.on('disconnect',()=>{
+
+  socket.on('disconnect', () => {
     console.log("User Disconnected!");
   })
 })
+// })
 
-
-
-http.listen(port,()=>{
+http.listen(port, () => {
   console.log("Congrats.... server is running on 3000 port!");
 })
