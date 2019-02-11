0bd2f9fc2aa18d4706e33a114d6b67db93e058af
intern <intern@admins-Mac-mini-2.local>
2019-02-11 18:53:30 +0530

    First

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const port = process.env.PORT || 3000
 app.get('/', (request, response) => {
   response.sendFile(__dirname + "/page.html")
 })
-
+let obj ={}
 // app.post('/',(req,res)=>{
 io.on('connection', (socket) => {
   console.log("User Connected!");
-  io.emit('chat',socket.handshake.address)
+  // io.emit('chat',socket.handshake.address)
+  socket.on('store_name',(name)=>{
+    obj[socket.handshake.address] = name
+  })
+
   socket.on('chat_message', (msg) => {
     console.log()
     console.log(`message: ${msg}`);
-    io.emit('chat', msg)
+    io.emit('chat', msg,obj[socket.handshake.address])
   })
 
   socket.on('disconnect', () => {
