72cf745afc4e3255f5696a28a894eea678065235
Parminder <parminder.singh01@jungleworks.com>
2019-03-28 16:48:00 +0530

    Single commit

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
-
-// io.on('connection',()=>{
-//   console.log('You are connected!');
-// }) 
-
-app.get('/',(request,response)=>{
-  response.sendFile(__dirname+"/page.html")
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + "/page.html")
 })
-
-io.on('connection',(socket)=>{
+let obj ={}
+// app.post('/',(req,res)=>{
+io.on('connection', (socket) => {
   console.log("User Connected!");
-  
-  socket.on('chat_message',(msg)=>{
-    console.log(`message: ${msg}` );
-    io.emit('chat',msg)
+  // io.emit('chat',socket.handshake.address)
+  socket.on('store_name',(name)=>{
+    obj[socket.handshake.address] = name
   })
-  
-  socket.on('disconnect',()=>{
+
+  socket.on('chat_message', (msg) => {
+    console.log()
+    console.log(`message: ${msg}`);
+    io.emit('chat', msg,obj[socket.handshake.address])
+  })
+
+  socket.on('disconnect', () => {
     console.log("User Disconnected!");
   })
 })
+// })
 
-
-
-http.listen(3000,()=>{
+http.listen(port, () => {
   console.log("Congrats.... server is running on 3000 port!");
 })
